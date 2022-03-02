310513c447f78b2f36d676b6bafdf86f4d142786
narumichi0710 <narumichi7373@gmail.com>
2022-03-02 20:45:51 +0900

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Regolith on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
